9876d66747a200a4c2b7827894e6eeb9d63b21cb
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-22 12:15:30 -0600

    add creation code for color__text

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const userColorDis = document.getElementById('userColor');
 const schemeP = document.getElementById('scheme-select');
 const userSchemeDis = document.getElementById('userScheme');
 const submitBtn = document.getElementById('create-scheme__btn');
+const textSection = document.querySelector('.text__wrapper');
 const baseURL = 'https://www.thecolorapi.com/scheme?';
 let user_color = '#000000';
 let user_scheme = 'monochrome';
@@ -16,9 +17,25 @@ schemeP.addEventListener('change', () => {
 });
 
 submitBtn.addEventListener('click', () => {
-  let url = baseURL + `hex=${user_color.slice(1)}` + `&mode=${user_scheme}` + `&count=6`;
+  let url =
+    baseURL +
+    `hex=${user_color.slice(1)}` +
+    `&mode=${user_scheme}` +
+    `&count=6`;
   console.log(url);
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      for (let i = 0; i < 6; i++) {
+        let article = document.createElement('article');
+        article.classList.add('color__text');
+        article.innerText = `
+          ${data.colors[i].hex.value}
+					${data.colors[i].name.value}
+        `;
+        console.log(article);
+        textSection.appendChild(article);
+      }
+      console.log(data.colors[0].hex.value);
+    });
 });
