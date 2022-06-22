aff30d4cfc620c99664433423c4416d898595407
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-22 11:10:33 -0600

    add listener to capture user scheme selection

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const colorP = document.getElementById('color-picker');
 const userColorDis = document.getElementById('userColor');
 const schemeP = document.getElementById('scheme-select');
+const userSchemeDis = document.getElementById('userScheme');
+const baseURL =  'https://www.thecolorapi.com/scheme?'
 
 colorP.addEventListener('input', () => {
   let user_color = colorP.value;
@@ -8,3 +10,11 @@ colorP.addEventListener('input', () => {
 		<h1>User Color is: ${user_color}</h1>
 `;
 });
+
+schemeP.addEventListener('change', () => {
+  let user_scheme = schemeP.value;
+  userSchemeDis.innerHTML = `
+   <h2>User Scheme is: ${user_scheme.toUpperCase()}</h2>
+`;
+});
+
