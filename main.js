081dd544b52363024f67a1c8f5e58bbcf5ddffb7
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-22 12:31:35 -0600

    add code to handle decreased selections (triad and quad) and create color__box articles

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const schemeP = document.getElementById('scheme-select');
 const userSchemeDis = document.getElementById('userScheme');
 const submitBtn = document.getElementById('create-scheme__btn');
 const textSection = document.querySelector('.text__wrapper');
+const colorSection = document.querySelector('.color__wrapper');
 const baseURL = 'https://www.thecolorapi.com/scheme?';
 let user_color = '#000000';
 let user_scheme = 'monochrome';
+let user_count = '6';
 
 colorP.addEventListener('input', () => {
   user_color = colorP.value;
@@ -14,28 +16,38 @@ colorP.addEventListener('input', () => {
 
 schemeP.addEventListener('change', () => {
   user_scheme = schemeP.value;
+  user_count = '6';
+  if (user_scheme === 'quad') {
+    user_count = 4;
+  } else if (user_scheme === 'triad') {
+    user_count = 3;
+  }
 });
 
 submitBtn.addEventListener('click', () => {
+  textSection.innerHTML = '';
+  colorSection.innerHTML = '';
   let url =
     baseURL +
     `hex=${user_color.slice(1)}` +
     `&mode=${user_scheme}` +
-    `&count=6`;
+    `&count=${user_count}`;
   console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < 6; i++) {
-        let article = document.createElement('article');
-        article.classList.add('color__text');
-        article.innerText = `
+        let text_article = document.createElement('article');
+        let color_article = document.createElement('article');
+        text_article.classList.add('color__text');
+        color_article.classList.add('color__box');
+        color_article.style.backgroundColor = `${data.colors[i].hex.value}`;
+        text_article.innerText = `
           ${data.colors[i].hex.value}
 					${data.colors[i].name.value}
         `;
-        console.log(article);
-        textSection.appendChild(article);
+        textSection.appendChild(text_article);
+        colorSection.appendChild(color_article);
       }
-      console.log(data.colors[0].hex.value);
     });
 });
